10677e0f67e9deb5bc8975dc896f038d26467633
lizqwerscott <lizqwerscott@163.com>
2024-10-05 15:01:45 +0800

    feat: add handle state

--- src/app.rs
+++ src/app.rs
@@ -186,6 +186,23 @@ impl ZipFiles {
     }
 }
 
+#[derive(Clone, Debug, Eq, PartialEq)]
+enum State {
+    NeedInit,
+    Running,
+    Finish,
+}
+
+impl fmt::Display for State {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            State::NeedInit => write!(f, "需要初始化"),
+            State::Running => write!(f, "正在解压"),
+            State::Finish => write!(f, "解压完成"),
+        }
+    }
+}
+
 pub struct ZipDive {
     input_path: PathBuf,
     output_path: PathBuf,
@@ -193,7 +210,7 @@ pub struct ZipDive {
     zip_files: Vec<ZipFiles>,
     now_run_zip_files: usize,
     auto_run: bool,
-    all_finish: bool,
+    state: State,
 }
 
 impl ZipDive {
@@ -206,7 +223,7 @@ impl ZipDive {
                 zip_files: Vec::new(),
                 now_run_zip_files: 0,
                 auto_run: false,
-                all_finish: false,
+                state: State::NeedInit,
             },
             Task::none(),
         )
@@ -230,9 +247,7 @@ impl ZipDive {
         match message {
             Message::InputPathChange(s) => {
                 let path = PathBuf::from(s.trim());
-                if path.exists() {
-                    self.input_path = path;
-                }
+                self.input_path = path;
                 Task::none()
             }
             Message::InputPathFileDialog => {
@@ -244,9 +259,7 @@ impl ZipDive {
             }
             Message::OutputPathChange(s) => {
                 let path = PathBuf::from(s.trim());
-                if path.exists() {
-                    self.output_path = path;
-                }
+                self.output_path = path;
                 Task::none()
             }
             Message::OutputPathFileDialog => {
@@ -261,43 +274,59 @@ impl ZipDive {
                 Task::none()
             }
             Message::Start => {
-                if self.now_run_zip_files > 0 {
-                    println!("已经处于运行状态");
-                } else {
-                    self.now_run_zip_files = 1;
+                match self.state {
+                    State::Running => {
+                        println!("已经处于运行状态");
+                    }
+                    _ => {
+                        // check path exist
+                        if !self.input_path.exists() || !self.output_path.exists() {
+                            println!("路径不存在");
+                            return Task::none();
+                        }
 
-                    // 创建第一层的输出目录
-                    let output_path = self.output_path.join("1");
-                    self.zip_files.push(ZipFiles::new(
-                        self.input_path.clone(),
-                        output_path,
-                        self.now_run_zip_files,
-                    ));
+                        self.now_run_zip_files = 1;
+                        self.state = State::Running;
+
+                        // 创建第一层的输出目录
+                        let output_path = self.output_path.join("1");
+                        self.zip_files.push(ZipFiles::new(
+                            self.input_path.clone(),
+                            output_path,
+                            self.now_run_zip_files,
+                        ));
+                    }
                 }
 
                 Task::none()
             }
             Message::Next => {
-                if !self.all_finish {
-                    if !self.auto_run {
-                        if let Some(last_zip_files) = self.zip_files.last() {
-                            match last_zip_files.state {
-                                ZipsHandleState::Finished => {
-                                    self.next_zip_files();
-                                }
-                                ZipsHandleState::EmptyZips => {
-                                    println!("已经搜索到最后一层，无法进行下一层解压");
-                                }
-                                _ => {
-                                    println!("上一层解压未完成，无法进行下一层解压");
+                match self.state {
+                    State::Finish => {
+                        println!("递归解压完成");
+                    }
+                    State::Running => {
+                        if !self.auto_run {
+                            if let Some(last_zip_files) = self.zip_files.last() {
+                                match last_zip_files.state {
+                                    ZipsHandleState::Finished => {
+                                        self.next_zip_files();
+                                    }
+                                    ZipsHandleState::EmptyZips => {
+                                        println!("已经搜索到最后一层，无法进行下一层解压");
+                                    }
+                                    _ => {
+                                        println!("上一层解压未完成，无法进行下一层解压");
+                                    }
                                 }
                             }
+                        } else {
+                            println!("处于自动运行模式，无需手动操作");
                         }
-                    } else {
-                        println!("处于自动运行模式，无需手动操作");
                     }
-                } else {
-                    println!("递归解压完成");
+                    State::NeedInit => {
+                        println!("需要开始解压");
+                    }
                 }
 
                 Task::none()
@@ -305,13 +334,17 @@ impl ZipDive {
             Message::ZipFileHandleProgress((id, progress)) => {
                 if let Some(zip_file) = self.zip_files.get_mut(id - 1) {
                     zip_file.progress(progress);
-                    if self.auto_run {
-                        match zip_file.state {
-                            ZipsHandleState::Finished => {
+
+                    match zip_file.state {
+                        ZipsHandleState::Finished => {
+                            if self.auto_run {
                                 self.next_zip_files();
                             }
-                            _ => {}
                         }
+                        ZipsHandleState::EmptyZips => {
+                            self.state = State::Finish;
+                        }
+                        _ => {}
                     }
                 }
                 Task::none()
@@ -328,35 +361,53 @@ impl ZipDive {
     }
 
     pub fn view(&self) -> Element<Message> {
-        let input_path_input = text_input(
-            "输入要处理的文件路径...",
-            self.input_path.display().to_string().as_str(),
-        )
-        .on_input(Message::InputPathChange)
-        .padding(10);
-        let input_path_button = button("select")
-            .padding(10)
-            .on_press(Message::InputPathFileDialog);
+        let (input_path_input, input_path_button) = if self.state == State::Running {
+            (
+                text_input(
+                    "输入要处理的文件路径...",
+                    self.input_path.display().to_string().as_str(),
+                ),
+                button("select"),
+            )
+        } else {
+            (
+                text_input(
+                    "输入要处理的文件路径...",
+                    self.input_path.display().to_string().as_str(),
+                )
+                .on_input(Message::InputPathChange),
+                button("select").on_press(Message::InputPathFileDialog),
+            )
+        };
 
-        let output_path_input = text_input(
-            "输入要导出的位置...",
-            &self.output_path.display().to_string().as_str(),
-        )
-        .on_input(Message::OutputPathChange)
-        .padding(10);
+        let (output_path_input, output_path_button) = if self.state == State::Running {
+            (
+                text_input(
+                    "输入要导出的位置...",
+                    &self.output_path.display().to_string().as_str(),
+                ),
+                button("select"),
+            )
+        } else {
+            (
+                text_input(
+                    "输入要导出的位置...",
+                    &self.output_path.display().to_string().as_str(),
+                )
+                .on_input(Message::OutputPathChange),
+                button("select").on_press(Message::OutputPathFileDialog),
+            )
+        };
 
-        let output_path_button = button("select")
-            .padding(10)
-            .on_press(Message::OutputPathFileDialog);
-
-        let password_input = text_input("输入默认压缩密码...", &self.password)
-            .on_input(Message::PasswordChange)
-            .padding(10);
+        let password_input =
+            text_input("输入默认压缩密码...", &self.password).on_input(Message::PasswordChange);
 
         let start_button = button("Start").on_press(Message::Start);
         let next_button = button("Next").on_press(Message::Next);
         let auto_run_checkbox =
             checkbox("AutoRun", self.auto_run).on_toggle(Message::AutoRunCheckboxToggled);
+
+        let state_show = text(format!("状态: {}", self.state)).shaping(text::Shaping::Advanced);
 
         let controls = row![
             column![
@@ -383,7 +434,7 @@ impl ZipDive {
                 ]
                 .align_y(Alignment::Center)
                 .spacing(10),
-                row![start_button, next_button, auto_run_checkbox]
+                row![state_show, start_button, next_button, auto_run_checkbox]
                     .align_y(Alignment::Center)
                     .spacing(10)
             ]
@@ -395,10 +446,12 @@ impl ZipDive {
         let show_zip_files: Element<Message> = if self.zip_files.is_empty() {
             center(text("没有压缩文件").shaping(text::Shaping::Advanced)).into()
         } else {
-            Row::with_children(self.zip_files.iter().map(ZipFiles::view)).into()
+            Row::with_children(self.zip_files.iter().map(ZipFiles::view))
+                .spacing(10)
+                .into()
         };
 
-        container(column![controls, show_zip_files])
+        container(column![controls, show_zip_files].spacing(10))
             .padding(10)
             .into()
     }
